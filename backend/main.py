94b20c87445ec4b4de5d1b391fd2f3dc51a6c81c
AxelMartin69 <75273577+AxelMartin69@users.noreply.github.com>
2022-11-30 21:17:29 +0200

    [#183924652] Backend returns token
    Backend returns security token when username and password are correct

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,43 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
 from database import engine, SessionLocal
 import models
-from datetime import datetime
-from sqlalchemy.orm import Session
+from datetime import datetime, timedelta
+
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "d04dc20b5a85b7a54e1cf9061a82afd12515cb2007e8ba0a1908e35f46e604aa"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+
+class Poem(BaseModel):
+    title: str
+    body: str
 
 
 models.Base.metadata.create_all(bind=engine)
 
 cors_origins = 'http://localhost:3000'
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
 
@@ -91,6 +120,42 @@ def get_db():
         db.close()
 
 
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=60*24)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def validate_token(token):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+        return token_data
+    except JWTError:
+        raise credentials_exception
+
+
 # Get all poems
 @app.get('/')
 async def get_poems(db: Session = Depends(get_db)):
@@ -103,3 +168,31 @@ async def get_poems(db: Session = Depends(get_db)):
         poems[i].author = db.execute(
             f'SELECT username FROM users WHERE id = {poems[i].owner_id}').fetchall()[0][0]
     return poems
+
+# Get token
+
+
+@ app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    # get user from db by username
+    user = db.query(models.User).filter(
+        models.User.username == form_data.username).first()
+
+    # authenticate user
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    if not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
